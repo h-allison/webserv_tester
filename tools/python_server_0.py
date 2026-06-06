d364105e39a948c1efbf6c7abb9cb1a1482efd14
Hilary Allison <165587952+hilary-allison@users.noreply.github.com>
2026-06-06 16:20:23 +0200

    feature(python server): re-use old python server code for new purpose of reading 42 testers tests, move to /tools

--- tools/python_server_0.py
+++ tools/python_server_0.py
@@ -6,7 +6,7 @@ import socket
 
 host = "127.0.0.1"
 port = 3490
-backlog = 10
+# backlog = 10
 
 # socket.socket (socket_family, socket_type, protocol=0)
 # family -- "AF_INET by default" .. no option for AF_UNSPEC?
@@ -15,26 +15,25 @@ backlog = 10
 # does 0 = TCP?
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', port)) # empty IP means listening socket
+server.bind((host, port))
 
-server.listen(5) # what is 5?
+server.listen()
 print("Python server is listening on port %s" %(port))
 
 # accept() returns BOTH
 # 1) a connection (usually, different socket on another port assigned by the kernel)
 # 2) address of the client
+c, addr = server.accept()
+print("Got connection from ", addr)
 
 while (1):
-	c, addr = server.accept(backlog)
-	print("Got connection from ", addr)
+	data = c.recv(2048).decode()
+	if not data:
+		break
+	print ("from client:\n")
+	print (str(data))
 
-	c.send(b'Thank you for connecting')
-	# what is b in this exmple?
-	# why does telnet ignore this char
-	# while python client treats as part
-	# of the message
-	c.close()
-
+c.close()
 
 # Sources:
 # https://www.tutorialspoint.com/python/python_socket_programming.htm
